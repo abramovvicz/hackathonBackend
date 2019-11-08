5a7a72c6640781729ffb1970644088d258fb08fe
taralalukasz <taralalukasz123@gmail.com>
2019-11-08 13:52:57 +0100

    added findAllRoutes endpoint in controller

--- src/main/java/backend/controller/BorsukiRouteController.java
+++ src/main/java/backend/controller/BorsukiRouteController.java
@@ -5,18 +5,27 @@ import backend.repository.BorsukiRouteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BorsukiRouteController {
     @Autowired
     private BorsukiRouteRepository locationRepository;
 
-    @RequestMapping("/save")
+    @PostMapping("/save")
     public ResponseEntity saveBorsukiRoute(@RequestBody BorsukiRoute borsukiRoute) {
         locationRepository.save(borsukiRoute);
         return ResponseEntity.ok(HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/find/all", produces = "application/json")
+    public ResponseEntity<List<BorsukiRoute>> getAllBorsukiRoute() {
+        List<BorsukiRoute> allBorsukiRoutes = locationRepository.findAll();
+        return new ResponseEntity<>(allBorsukiRoutes, HttpStatus.OK);
     }
 }
