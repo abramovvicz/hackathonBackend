b52c77fa5ea802a590bd5ac370b40f82bc5b1ed4
taralalukasz <taralalukasz123@gmail.com>
2019-11-08 12:32:30 +0100

    added location controller

--- src/main/java/backend/model/Location.java
+++ src/main/java/backend/model/Location.java
@@ -1,4 +1,5 @@
 package backend.model;
 
 public class Location {
+
 }
--- src/main/java/backend/repository/LocationRepository.java
+++ src/main/java/backend/repository/LocationRepository.java
@@ -1,4 +1,8 @@
 package backend.repository;
 
-public class LocationRepository {
+import backend.model.Location;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LocationRepository extends MongoRepository<Location, String> {
+    
 }
